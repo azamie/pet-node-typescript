47932f7c1926c89f2ee866104ef9fd36bc437105
azamie <ishii.kami2001@gmail.com>
2022-11-28 21:11:04 +0700

    Setup Jest for project

---
+++ src/__test__/pet.service.test.ts
@@ -0,0 +1,9 @@
+import PetService from '@/resources/pet/pet.service';
+
+let petService = new PetService();
+
+describe('PetService', () => {
+  it('should return pet list', () => {
+    
+  });
+});
